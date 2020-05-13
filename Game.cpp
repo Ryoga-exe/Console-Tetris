c5b97c64500a4a1ce4da128de6edf499ea2d47b1
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-05-13 23:55:18 +0900

    Update SRS (Super Rotation System)

--- Game.cpp
+++ Game.cpp
@@ -350,26 +350,45 @@ bool Game::MinoRotate(bool isClockWise) {
 			if (isClockWise) {
 				if (m_currentMino.minoAngle == 0) {
 					bufPos.X -= 2;
-					if (IsHit(bufPos, buf)) {
+					if (!IsHit(bufPos, buf)) {
+						m_currentMino = buf;
+						m_currentMinoPos = bufPos;
+						if (m_lockDown()) m_lockDown++;
+						return false;
+					}
+					else {
 						bufPos.X = m_currentMinoPos.X + 1;
-						if (IsHit(bufPos, buf)) {
+						if (!IsHit(bufPos, buf)) {
+							m_currentMino = buf;
+							m_currentMinoPos = bufPos;
+							if (m_lockDown()) m_lockDown++;
+							return false;
+						}
+						else {
 							bufPos.X = m_currentMinoPos.X - 2;
 							bufPos.Y += 1;
-							if (IsHit(bufPos, buf)) {
+							if (!IsHit(bufPos, buf)) {
+								m_currentMino = buf;
+								m_currentMinoPos = bufPos;
+								if (m_lockDown()) m_lockDown++;
+								return false;
+							}
+							else {
 								bufPos.X = m_currentMinoPos.X + 1;
 								bufPos.Y = m_currentMinoPos.Y - 2;
-								if (IsHit(bufPos, buf)) {
+								if (!IsHit(bufPos, buf)) {
+									m_currentMino = buf;
+									m_currentMinoPos = bufPos;
+									if (m_lockDown()) m_lockDown++;
+									return false;
+								}
+								else {
 									return true;
 								}
 							}
 						}
 					}
 				}
-				m_currentMino = buf;
-				m_currentMinoPos = bufPos;
-				if (m_lockDown()) m_lockDown++;
-				return false;
-
 				if (m_currentMino.minoAngle == 1) {
 					bufPos.X -= 1;
 					if (!IsHit(bufPos, buf)) {
